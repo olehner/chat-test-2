1208fc8e6b34036179a81388049b5acee076d416
Oliver Lehner <lehner.oliver@gmail.com>
2017-04-05 09:41:18 +0200

    no figaro this time

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+    root 'users#index'
+  end
+
+  unauthenticated :user do
+    devise_scope :user do
+      get "/" => "devise/sessions#new"
+    end
+  end
+
+  resources :conversations do
+    resources :messages
+  end
 end
